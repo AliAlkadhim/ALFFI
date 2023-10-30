d7e7f0e4b57ac2d3caad356329ce6ac96b1159ca
AliAlkadhim <aa18dg@my.fsu.edu>
2023-10-29 21:59:37 -0500

    add on off

--- On_Off_HEP/setup.sh
+++ On_Off_HEP/setup.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
-conda activate ALFFI
+# conda activate ALFFI
 export ALFFI_ON_OFF=`pwd`
 export ALFFI_ON_OFF_DATA=$ALFFI_ON_OFF/data
 export ALFFI_ON_OFF_SRC=$ALFFI_ON_OFF/src
 export ALFFI_ON_OFF_MODELS=$ALFFI_ON_OFF/models
 export ALFFI_ON_OFF_IMAGES=$ALFFI_ON_OFF/images
+
+# export deps with conda env create -n ALFFI python=3.9 scipy numpy pandas torch numba matplotlib joblib importlib
